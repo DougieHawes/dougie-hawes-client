af7842b1d3587ebbdc54a0a45313bfd789f20943
dougiehawes <dougiehawes@hotmail.com>
2025-02-17 12:48:06 +0000

    added items to the skills page

--- src/components/routes/public/skillsArray.js
+++ src/components/routes/public/skillsArray.js
@@ -1,19 +1,24 @@
-import HTML from "../../../display/media/images/skills/html.png";
 import Bootstrap from "../../../display/media/images/skills/bootstrap.png";
 import CSS from "../../../display/media/images/skills/css.png";
 import FramerMotion from "../../../display/media/images/skills/framermotion.png";
+import GraphQl from "../../../display/media/images/skills/graphql.png";
+import GSAP from "../../../display/media/images/skills/gsap.png";
+import HTML from "../../../display/media/images/skills/html.png";
 import JavaScript from "../../../display/media/images/skills/javascript.png";
+import JWT from "../../../display/media/images/skills/jwt.png";
 import Less from "../../../display/media/images/skills/less.png";
 import Materialize from "../../../display/media/images/skills/materialize.png";
-import Next from "../../../display/media/images/skills/next.png";
 import Mongo from "../../../display/media/images/icons/mongodb-logo.png";
+import Next from "../../../display/media/images/skills/next.png";
+import Node from "../../../display/media/images/icons/node-logo.png";
 import Express from "../../../display/media/images/icons/express-logo.png";
 import React from "../../../display/media/images/icons/react-logo.png";
 import ReactNative from "../../../display/media/images/skills/react-native.png";
 import Sass from "../../../display/media/images/skills/sass.png";
+import Tailwind from "../../../display/media/images/skills/tailwind.png";
 import Three from "../../../display/media/images/skills/three.png";
 import Typescript from "../../../display/media/images/skills/typescript.png";
-import Node from "../../../display/media/images/icons/node-logo.png";
+import Wordpress from "../../../display/media/images/skills/wordpress.png";
 
 const skillsArray = [
   { title: "HTML5", image: HTML, level: "expert", details: "lorem ipsum..." },
@@ -67,6 +72,12 @@ const skillsArray = [
     details: "lorem ipsum...",
   },
   {
+    title: "JSON WebToken",
+    image: JWT,
+    level: "skilled",
+    details: "lorem ipsum...",
+  },
+  {
     title: "FramerMotion.js",
     image: FramerMotion,
     level: "skilled",
@@ -78,8 +89,13 @@ const skillsArray = [
     level: "skilled",
     details: "lorem ipsum...",
   },
-  { title: "GSAP", level: "skilled", details: "lorem ipsum..." },
-  { title: "Wordpress", level: "skilled", details: "lorem ipsum..." },
+  { title: "GSAP", image: GSAP, level: "skilled", details: "lorem ipsum..." },
+  {
+    title: "Wordpress",
+    image: Wordpress,
+    level: "skilled",
+    details: "lorem ipsum...",
+  },
   {
     title: "sass",
     image: Sass,
@@ -94,6 +110,7 @@ const skillsArray = [
   },
   {
     title: "Tailwind.css",
+    image: Tailwind,
     level: "proficient",
     details: "lorem ipsum...",
   },
@@ -107,6 +124,12 @@ const skillsArray = [
     title: "Materialize.css",
     image: Materialize,
     level: "proficient",
+    details: "lorem ipsum...",
+  },
+  {
+    title: "GraphQL",
+    image: GraphQl,
+    level: "skilled",
     details: "lorem ipsum...",
   },
 ];
