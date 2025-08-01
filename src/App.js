7e9a7862c6da0e7defd8decad3e34bfcc58a8b30
dougiehawes <dougiehawes@hotmail.com>
2025-08-01 17:04:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+import Header from "./components/layout/Header";
+
+import "./display/style/style.scss";
+import DarkmodeToggle from "./components/layout/DarkmodeToggle";
+
+const App = () => {
+  const [darkmode, setDarkmode] = useState(false);
+
+  return (
+    <div className={`app ${darkmode ? "app-darkmode" : "app-lightmode"}`}>
+      <Header />
+      <DarkmodeToggle onClick={() => setDarkmode(!darkmode)} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,16 @@
+import "./style.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-title-container">
+        <h1 className="header-title">
+          <span className="header-initial">D</span>ougie
+          <span className="header-initial">H</span>awes
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
